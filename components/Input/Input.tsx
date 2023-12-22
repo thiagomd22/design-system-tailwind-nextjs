6d5ea30055741d21374eea794e6983cc4260c2b5
Thiago Meireles <thiago22md@gmail.com>
2023-12-22 18:12:18 -0300

    input_update

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -16,11 +16,12 @@ const InputOrTextArea = (props: InputProps) => {
 
 const Input = ({ label, className, ...rest}: InputProps) => {
   return <div className="relative">
-    {label && <label className="absolute">{label}</label>}
+    {label && <label className="absolute pt-1 pl-3 text-xs">{label}</label>}
     <InputOrTextArea
       className={`
-        bg-dark text-gray-primary
+        bg-dark text-gray-primary rounded-md p-3
         disabled:bg-disabled disabled:border-2 disabled:border-disabled
+        ${label && 'pt-5'}
         ${className}
       `}
       {...rest}
